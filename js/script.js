77e26c1f666f96485edbc20c5f4e68142965d076
Luis Carlos <luiscbravo94@gmail.com>
2024-07-12 17:09:51 +0100

    code optimized

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
-const mainContaner = document.querySelector('.main-cointainer')
+
 const navbarLogo = document.querySelector('.navbar-logo')
 const darkMode = document.querySelector('.darkMode')
 const title = document.querySelector('.title')
 const article = document.querySelector('.article')
 const imgDarkMode = document.querySelector('.navbar-dark--imgMove')
 const imgDarkModeContainer = document.querySelector('.navbar-dark--img-background')
-
+const body = document.querySelector('.body')
+console.log(body);
 
 imgDarkModeContainer.addEventListener('click', () => {
     
 
     if (!imgDarkModeContainer.classList.contains('moveRight')) {
-        mainContaner.style.background ='black'
+        
+        body.classList.add('background')
+        darkMode.classList.add('white')
         navbarLogo.classList.add('yellow')
         title.classList.add('yellow')
         article.classList.add('white')
-        darkMode.classList.add('white')
-        
-        imgDarkModeContainer.classList.add('moveRight')
         imgDarkMode.src = '../assets/img/luna.png'
+        imgDarkModeContainer.classList.add('moveRight')
     }else{
-        mainContaner.style.background ='white'
+        
+        body.classList.remove('background')
         darkMode.classList.remove('white')
         navbarLogo.classList.remove('yellow')
         title.classList.remove('yellow')
         article.classList.remove('white')
-        
         imgDarkMode.src = '../assets/img/sol.png'
         imgDarkModeContainer.classList.remove('moveRight')
     }
